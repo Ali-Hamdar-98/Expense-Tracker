5ae3bfa7f21e8af34049c48d07795cf04e288528
Mico CS <ali.hamdar.cs@gmail.com>
2023-07-19 02:46:02 +0300

    adding initial Jq code

---
+++ style.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+    const expenses = [];
+    let totalAmount = 0;
+
+    $("#add").click(function() {
+        const category = $("#category").val();
+        const amount = parseFloat($("#amount").val());
+        const date = $("#date").val();
+
+    
+    });
+});
